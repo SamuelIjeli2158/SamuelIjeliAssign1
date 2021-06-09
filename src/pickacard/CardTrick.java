f991a88d031ecd60238fac5fa4522897f156b5ce
sam <sam@DESKTOP-GRK3HGG>
2021-06-08 23:46:30 -0300

    asked user to select a card

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -19,6 +19,7 @@ public class CardTrick {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            System.out.println("please pick a card");
         }
 
         //insert code to ask the user for Card value and suit, create their card
