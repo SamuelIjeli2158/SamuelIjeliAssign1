f3f9562c5824bc39bf6a81a9017fa79a6a29cdb1
sam <sam@DESKTOP-GRK3HGG>
2021-06-08 23:55:24 -0300

    added scanner class for user input

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -1,5 +1,7 @@
 package pickacard;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
@@ -12,19 +14,27 @@ package pickacard;
 public class CardTrick {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
         Card[] magicHand = new Card[7];
 
         for (int i = 0; i < magicHand.length; i++) {
+            System.out.println("please pick a card");
+            String randomNumber = sc.nextLine();
             Card c = new Card();
+           
+            
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            System.out.println("please pick a card");
+            
         }
 
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+    }
+
+    public CardTrick() {
     }
 
 }
